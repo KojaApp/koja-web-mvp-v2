3e4994dcf58ea48c9ed7c1f5de81d274ab3388f1
KojaApp <admin@trykoja.com>
2024-12-17 13:30:42 +0000

    1st committed

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Merriweather, Montserrat } from 'next/font/google';
+ 
+export const merriweather = Merriweather({
+    subsets: ['latin'],
+    weight: '300'
+});
+ 
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    weight: '400'
+});
---
+++ app/ui/koja-logo.tsx
@@ -0,0 +1,13 @@
+import { DevicePhoneMobileIcon } from '@heroicons/react/24/outline';
+import { montserrat } from 'app/ui/fonts';
+
+export default function KojaLogo() {
+  return (
+    <div
+      className={`${montserrat.className} flex flex-row items-center leading-none text-white`}
+    >
+      <DevicePhoneMobileIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Koja</p>
+    </div>
+  );
+}
