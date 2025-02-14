3d3391f529240409320b8ef3a09700eafd40b44d
KojaApp <admin@trykoja.com>
2025-02-14 16:23:08 +0000

    updated sidenav layout

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/koja-logo';
+import KojaLogo from '@/app/ui/koja-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
 
@@ -8,14 +8,14 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-[#0A2540]">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-[#0A2540] p-4 md:h-40"
+        className="mb-2 flex h-20 justify-start rounded-md bg-[#0A2540] p-4 md:h-30"
         href="/"
       >
-        <div className="w-32 md:w-40">
-          <AcmeLogo />
+        <div className="w-32 md:w-40 border-solid">
+          <KojaLogo />
         </div>
       </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <div className="flex grow pr-6 flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md md:block"></div>
         <form 
