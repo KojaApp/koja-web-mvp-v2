93619a1fb571b1354209c4fd8c32892e300fd06f
KojaApp <admin@trykoja.com>
2025-02-14 17:07:50 +0000

    .

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -15,7 +15,7 @@ export default function SideNav() {
           <KojaLogo />
         </div>
       </Link>
-      <div className="flex grow pr-6 flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <div className="flex grow pr-2 flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md md:block"></div>
         <form 
