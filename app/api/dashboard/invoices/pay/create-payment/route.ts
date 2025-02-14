d156d511617518888313c32de8514b7dd7b1decd
KojaApp <admin@trykoja.com>
2025-02-14 13:48:08 +0000

    .

--- app/api/dashboard/invoices/pay/create-payment/route.ts
+++ app/api/dashboard/invoices/pay/create-payment/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
     const payload = {
       applicationUserId: applicationKey,
       institutionId,
-      callback: "https://koja-web-mvp-v2.vercel.app/api/dashboard/invoices/pay/callback/",
+      callback: "https://auth.yapily.com/",
       paymentRequest: {
         type: "DOMESTIC_PAYMENT",
         reference: `Payment-${outboundChildPaymentRef}`.substring(0, 18),
